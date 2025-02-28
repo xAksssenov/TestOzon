b3a452e3f24d46f857a8adbc3baca94306a5110c
xAsssenov <aks.kirill32@gmail.com>
2025-02-27 20:42:17 -0800

    feat: add error

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   const ringBlock = document.getElementById("ringBlock");
   const ringProgress = document.getElementById("ringMain");
+  const errorRingValue = document.getElementById("errorRingValue");
 
   const valueInput = document.getElementById("valueInput");
   const animateToggle = document.getElementById("animateToggle");
   const hideToggle = document.getElementById("hideToggle");
+
+  function checkValue(value) {
+    return value >= 0 && value <= 100;
+  }
 
   function updateProgress(value) {
     const length = 2 * Math.PI * 60;
@@ -13,13 +18,35 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   valueInput.addEventListener("input", (e) => {
-    let value = Math.min(100, Math.max(0, e.target.value));
-    updateProgress(value);
+    let value = Number(e.target.value);
+
+    if (checkValue(value)) {
+      errorRingValue.classList.remove("progress__visible");
+      updateProgress(value);
+    } else {
+      errorRingValue.classList.add("progress__visible");
+    }
   });
 
   animateToggle.addEventListener("change", (e) => {
-    if (e.target.checked) ringProgress.classList.add();
-    else ringProgress.classList.remove();
+    // let animationInterval;
+    // let currentValue = 0;
+    // valueInput.value = 0;
+    // if (e.target.checked) {
+    //   updateProgress(0);
+    //   valueInput.disabled = true;
+    //   animationInterval = setInterval(() => {
+    //     currentValue += 1;
+    //     updateProgress(currentValue);
+    //     if (currentValue >= 100) {
+    //       clearInterval(animationInterval);
+    //     }
+    //   }, 10);
+    // } else {
+    //   clearInterval(animationInterval);
+    //   updateProgress(0);
+    //   valueInput.disabled = false;
+    // }
   });
 
   hideToggle.addEventListener("change", (e) => {
