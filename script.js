4a1533c9fc2e4bbd8ef51b053523f2934bcd3412
xAsssenov <aks.kirill32@gmail.com>
2025-02-27 16:50:44 -0800

    feat: start with test-case

---
+++ script.js
@@ -0,0 +1,5 @@
+const progressCircle = document.querySelector(".progress");
+const valueInput = document.getElementById("valueInput");
+const animateToggle = document.getElementById("animateToggle");
+const hideToggle = document.getElementById("hideToggle");
+const progressBlock = document.getElementById("progressBlock");
